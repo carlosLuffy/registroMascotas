a9bc461839a5fdb50d9c28107de9b820f0f07b4f
carlos o milton <carlos o milton@192.168.1.9>
2018-06-16 21:07:03 -0300

    conexion y base de datos

---
+++ src/registromascotas/Coneccion.java
@@ -0,0 +1,53 @@
+
+package registromascotas;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author carlos o milton
+ */
+public class Coneccion {
+    
+     private String url = "jdbc:mysql://localhost:3306/baseDeDatos";
+    private String login = "root"; //Administrador de MySQL
+    private String password = "";
+    private Connection cnx = null;
+    private Statement sttm = null;
+    private ResultSet rst = null;
+    
+       public void UID(String sql) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection(url, login, password);
+            sttm = cnx.createStatement();
+            sttm.executeUpdate(sql); //statement
+        } catch (ClassNotFoundException c) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + c.getMessage());
+            System.exit(1); //salir de aplicación
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+     public ResultSet getValores(String sql) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection(url, login, password);
+            sttm = cnx.createStatement();
+            rst = sttm.executeQuery(sql);  //resultset
+        } catch (ClassNotFoundException c) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + c.getMessage());
+            System.exit(1);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "ERROR: " + e.getMessage());
+            System.exit(1);
+        } 
+        return rst;
+        
+    }
+    
+}
---
+++ src/registromascotas/RegistroMascotas.java
@@ -0,0 +1,17 @@
+
+package registromascotas;
+
+/**
+ *
+ * @author carlos o milton
+ */
+public class RegistroMascotas {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+    
+}
